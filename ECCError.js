bf29c6faa88ba3676979a6bb0cdff915b9415c7f
Amedeo Zucchetti <amedeo.zucchetti@welld.ch>
2021-02-18 17:25:33 +0100

    feat: uniform error codes for iOS

--- ECCError.js
+++ ECCError.js
@@ -31,10 +31,39 @@ const AndroidErrorCode = {
 };
 
 /**
- * Check: TODO
+ * Check: https://developer.apple.com/documentation/security/1542001-security_framework_result_codes
  */
 const IOSErrorCode = {
-  // TODO.
+  '-128': ErrorCode.Canceled,               // errSecUserCanceled: User canceled the operation.
+  '-25291': ErrorCode.BiometryNotAvailable, // errSecNotAvailable: No keychain is available. You may need to restart your computer.
+  '-25292': ErrorCode.Generic,              // errSecReadOnly: This keychain cannot be modified.
+  '-25293': ErrorCode.LockoutTemporarily,   // errSecAuthFailed: The user name or passphrase you entered is not correct.
+  '-25294': ErrorCode.BiometryNotAvailable, // errSecNoSuchKeychain: The specified keychain could not be found.
+  '-25295': ErrorCode.BiometryNotAvailable, // errSecInvalidKeychain: The specified keychain is not a valid keychain file.
+  '-25296': ErrorCode.BiometryNotAvailable, // errSecDuplicateKeychain: A keychain with the same name already exists.
+  '-25297': ErrorCode.Generic,              // errSecDuplicateCallback: The specified callback function is already installed.
+  '-25298': ErrorCode.Generic,              // errSecInvalidCallback: The specified callback function is not valid.
+  '-25299': ErrorCode.BiometryNotAvailable, // errSecDuplicateItem: The specified item already exists in the keychain.
+  '-25300': ErrorCode.BiometryNotAvailable, // errSecItemNotFound: The specified item could not be found in the keychain.
+  '-25301': ErrorCode.Generic,              // errSecBufferTooSmall: There is not enough memory available to use the specified item.
+  '-25302': ErrorCode.Generic,              // errSecDataTooLarge: This item contains information which is too large or in a format that cannot be displayed.
+  '-25303': ErrorCode.Generic,              // errSecNoSuchAttr: The specified attribute does not exist.
+  '-25304': ErrorCode.BiometryNotAvailable, // errSecInvalidItemRef: The specified item is no longer valid. It may have been deleted from the keychain.
+  '-25305': ErrorCode.BiometryNotAvailable, // errSecInvalidSearchRef: Unable to search the current keychain.
+  '-25306': ErrorCode.BiometryNotAvailable, // errSecNoSuchClass: The specified item does not appear to be a valid keychain item.
+  '-25307': ErrorCode.BiometryNotAvailable, // errSecNoDefaultKeychain: A default keychain could not be found.
+  '-25308': ErrorCode.Generic,              // errSecInteractionNotAllowed: User interaction is not allowed.
+  '-25309': ErrorCode.Generic,              // errSecReadOnlyAttr: The specified attribute could not be modified.
+  '-25310': ErrorCode.BiometryNotAvailable, // errSecWrongSecVersion: This keychain was created by a different version of the system software and cannot be opened.
+  '-25311': ErrorCode.BiometryNotAvailable, // errSecKeySizeNotAllowed: This item specifies a key size which is too large or too small.
+  '-25312': ErrorCode.Generic,              // errSecNoStorageModule: A required component (data storage module) could not be loaded. You may need to restart your computer.
+  '-25313': ErrorCode.Generic,              // errSecNoCertificateModule: A required component (certificate module) could not be loaded. You may need to restart your computer.
+  '-25314': ErrorCode.Generic,              // errSecNoPolicyModule: A required component (policy module) could not be loaded. You may need to restart your computer.
+  '-25315': ErrorCode.Generic,              // errSecInteractionRequired: User interaction is required, but is currently not allowed.
+  '-25316': ErrorCode.Generic,              // errSecDataNotAvailable: The contents of this item cannot be retrieved.
+  '-25317': ErrorCode.Generic,              // errSecDataNotModifiable: The contents of this item cannot be modified.
+  '-25318': ErrorCode.Generic,              // errSecCreateChainFailed: One or more certificates required to validate this certificate cannot be found.
+  '-25319': ErrorCode.Generic,              // errSecInvalidPrefsDomain: The specified preferences domain is not valid.
 };
 
 export default class ECCError extends Error {
